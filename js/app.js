239efaa2a7b08cb21ee5be10e9ce7d459849e2c8
GalisGalisteo <galis.travelling@gmail.com>
2023-10-20 08:37:21 +0200

    ex finished

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-// Implementar solución aquí
+const randomHexColor = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+};
 
+const spanHexValue = document.querySelector('#hex-value');
+
+const changeBgColor = () => {
+    const hexColor = randomHexColor();
+    document.body.style.background = hexColor;
+    spanHexValue.textContent = hexColor;
+}
